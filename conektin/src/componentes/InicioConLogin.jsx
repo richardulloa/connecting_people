f021f1683e7aa9fba12db2da73c115c9f39a8188
Krutzzio <gerard.costa.curr@gmail.com>
2023-12-01 19:32:22 +0100

    InicioConLogin

--- conektin/src/componentes/InicioConLogin.jsx
+++ conektin/src/componentes/InicioConLogin.jsx
@@ -6,11 +6,38 @@ import Navegador from "./Navegador";
 const InicioConLogin = () => {
 
     return (
-        <div>
-            <Navegador />
+        <div className="contenido-inicio">
+            <header>
+                <Navegador />
+            </header>
+
+            <main className="contenido">
+                <section className="secciones proximos-eventos">
+                    <h2>Proximos Eventos</h2>
+                </section>
+
+                <section className="secciones tusEventos">
+                    <h2>Tus Eventos</h2>
+                </section>
+
+                <div className="eventosFamilias">
+                    <section className="secciones eventos-ciudad">
+                        <h2>Eventos Ciudad</h2>
+                    </section>
+
+                    <section className="secciones tusFamilias">
+                        <h2>Tus Familias</h2>
+                    </section>
+                </div>
+
+                <section className="secciones tradiciones">
+                    <h2>Tradiciones</h2>
+                </section>
+
+
+            </main>
         </div>
     )
-
 }
 
 export default InicioConLogin
