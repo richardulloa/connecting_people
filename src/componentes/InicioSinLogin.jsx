f8e4a1bafaa6c62e5212622fff45adabf73e44d5
richardulloa <richardjulloa1@gmail.com>
2023-12-01 09:44:21 +0100

    estructura pagina inicial

---
+++ src/componentes/InicioSinLogin.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import '../css/InicioSinLogin.css'
+
+const InicioSinLogin = () => {
+
+  return (
+
+    <div className="Nologin">
+      <div className="cabecera">
+        <div>
+          <img className="imglogo" src="https://th.bing.com/th/id/OIP.BSdNpNqWXmy1WQ0o0qtRCwHaFy?rs=1&pid=ImgDetMain" />
+        </div>
+        <div className="botones">
+          <button className="btn1">
+            Iniciar Sesion
+          </button>
+          <button className="btn2">
+            Registrate
+          </button>
+        </div>
+      </div>
+      <div className="contenido">
+        <section className="contenido1">
+          <h2>Titulo Descripción</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
+            odit distinctio iusto veniam voluptatibus possimus quis officia
+            repellendus quaerat! Officiis ipsa dolore necessitatibus commodi
+            eligendi adipisci? Nostrum earum repellendus quia?
+          </p>
+          <img src="https://i.kinja-img.com/image/upload/c_fit,q_60,w_1315/290c1a587fb5e6a9ac97ee526ff8d6c7.jpg" />
+        </section>
+        <section className="contenido2">
+          <h2>Titulo Lema</h2>
+          <div className="objetivos">
+            <article>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                laborum consequuntur cum facilis officiis! Excepturi molestias,
+                sit perferendis facilis, perspiciatis necessitatibus debitis alias
+                modi distinctio voluptates sequi at illum? Aliquid.
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                laborum consequuntur cum facilis officiis! Excepturi molestias,
+                sit perferendis facilis, perspiciatis necessitatibus debitis alias
+                modi distinctio voluptates sequi at illum? Aliquid.
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                laborum consequuntur cum facilis officiis! Excepturi molestias,
+                sit perferendis facilis, perspiciatis necessitatibus debitis alias
+                modi distinctio voluptates sequi at illum? Aliquid.
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                laborum consequuntur cum facilis officiis! Excepturi molestias,
+                sit perferendis facilis, perspiciatis necessitatibus debitis alias
+                modi distinctio voluptates sequi at illum? Aliquid.
+              </p>
+            </article>
+            <article>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                laborum consequuntur cum facilis officiis! Excepturi molestias,
+                sit perferendis facilis, perspiciatis necessitatibus debitis alias
+                modi distinctio voluptates sequi at illum? Aliquid.
+              </p>
+            </article>
+          </div>
+        </section>
+      </div>
+      <h3>Siguenos</h3>
+      <div className="pie">
+        <div className='enlacesimg'>
+          <img />
+          <img />
+          <img />
+          <img />
+        </div>
+        <div className='enlacespag'>
+          <a>Iniciar Sesión</a>
+          <a>Registrate</a>
+          <a>Ayuda</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InicioSinLogin;
