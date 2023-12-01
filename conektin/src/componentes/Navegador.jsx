f500a8aa61646f0daaeabfa0a23f1c9eef3508b5
Krutzzio <gerard.costa.curr@gmail.com>
2023-12-01 18:21:02 +0100

    Navegador provisional

--- conektin/src/componentes/Navegador.jsx
+++ conektin/src/componentes/Navegador.jsx
@@ -2,24 +2,27 @@ import React from "react";
 import './css/Navegador.css'
 import ListIcon from '@mui/icons-material/List';
 import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 const Navegador = () => {
 
     return (
-        <nav className="navegador">
+        <header className="navegador">
             <img className="logo-navegador" src="./logo192.png" alt="Logo" />
             <section className="barra-busqueda">
                 <div className="lista">
-                    <div className="label-barra"> <ListIcon /> <span>Lista intereses</span> </div>
+                    <div className="label-barra"> <ListIcon /> <span className="texto-lista">Lista intereses</span> </div>
                 </div>
                 <div className="buscar">
                     <input id="barra" className="input-barra" type="text" placeholder="Buscar" />
                     <div className="label-barra"> <SearchIcon /> </div>
                 </div>
             </section>
-            <h1>ESTO ES NAVEGADOR</h1>
-        </nav>
+            <section className="perfil">
+                <div className="foto-perfil"><AccountCircleIcon fontSize="inherit"/></div>
+            </section>
+        </header>
     )
 }
 
