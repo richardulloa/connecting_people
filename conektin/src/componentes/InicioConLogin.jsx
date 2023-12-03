7621ef5d99448802926958736330c676ca49ff44
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2023-12-03 12:14:06 +0100

    InicioConLogin Completado

--- conektin/src/componentes/InicioConLogin.jsx
+++ conektin/src/componentes/InicioConLogin.jsx
@@ -13,39 +13,53 @@ const InicioConLogin = () => {
             </header>
 
             <main className="contenido">
-                <Link to={"/"} className="box-container">
-                    <div className="box">
-                        <div className="background-image" style={{ backgroundImage: `url("https://img.freepik.com/foto-gratis/grupo-diverso-amigos-disparar-verano_53876-47015.jpg?w=1380&t=st=1701516264~exp=1701516864~hmac=0d3acfe6acbb5d0bffea85f2894eaeea4ecf3f695238290dd3d8c931395cae14")` }}></div>
+
+                <Link to={"/"} className="secciones proximos-eventos">
+                    <div className="caja">
+                        <div className="image pe"></div>
                         <div className="overlay">
                             <p className="text">Proximos Eventos</p>
                         </div>
                     </div>
                 </Link>
 
-
-
-
-                <Link className="secciones proximos-eventos">
-                    <h2>Proximos Eventos</h2>
+                <Link to={"/"} className="secciones tus-eventos">
+                    <div className="caja">
+                        <div className="image te"></div>
+                        <div className="overlay">
+                            <p className="text">Tus Eventos</p>
+                        </div>
+                    </div>
                 </Link>
 
-                <section className="secciones tusEventos">
-                    <h2>Tus Eventos</h2>
+                <section className="eventos-familias">
+                    <Link to={"/"} className="secciones eventos-ciudad ">
+                        <div className="caja">
+                            <div className="image ec"></div>
+                            <div className="overlay">
+                                <p className="text">Eventos Ciudad</p>
+                            </div>
+                        </div>
+                    </Link>
+
+                    <Link to={"/"} className="secciones tus-familias">
+                        <div className="caja">
+                            <div className="image tf"></div>
+                            <div className="overlay">
+                                <p className="text">Tus Familias</p>
+                            </div>
+                        </div>
+                    </Link>
                 </section>
 
-                <div className="eventosFamilias">
-                    <section className="secciones eventos-ciudad">
-                        <h2>Eventos Ciudad</h2>
-                    </section>
-
-                    <section className="secciones tusFamilias">
-                        <h2>Tus Familias</h2>
-                    </section>
-                </div>
-
-                <section className="secciones tradiciones">
-                    <h2>Tradiciones</h2>
-                </section>
+                <Link to={"/"} className="secciones tradiciones">
+                    <div className="caja">
+                        <div className="image tc"></div>
+                        <div className="overlay">
+                            <p className="text">Tradiciones Ciudad</p>
+                        </div>
+                    </div>
+                </Link>
 
 
             </main>
